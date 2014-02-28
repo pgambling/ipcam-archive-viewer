026b0d914331f90b04fc3c67957882923d35ab54
Phil Gambling <pgambling@gmail.com>
2014-02-27 22:08:21 -0600

    checkpoint

--- src-cljs/app/main.cljs
+++ src-cljs/app/main.cljs
@@ -2,14 +2,13 @@
   (:require-macros [hiccups.core :as hiccups])
   (:require
     [hiccups.runtime :as hiccupsrt])
-  (:use [jayq.core :only [$ document-ready html on attr]]))
+  (:use [jayq.core :only [$ document-ready html on attr prop text]]))
 
 ;;------------------------------------------------------------------------------
 ;; Atoms
 ;;------------------------------------------------------------------------------
 
-(def snapshot-list (atom nil))
-(def snapshot-index (atom nil)) ; can this be derived from current time, quickly?
+(def snapshots (atom nil))
 (def selected-time (atom nil))
 (def url-update-timout (atom nil)) ; This probably doesn't need to be atom, maybe be clojure feature that handles this
 
@@ -71,15 +70,28 @@
 (defn set-minutes! [new-mins]
   (swap! selected-time doto (.setMinutes new-mins)))
 
-(defn sync-time-with-snapshot! []
-  (swap! selected-time #(js/Date. (:time @snapshot-index))))
+(defn use-index-time! [index]
+  (swap! selected-time #(-> (@snapshot-list index) :time Date.)))
 
 ;;------------------------------------------------------------------------------
 ;; Watchers
 ;;------------------------------------------------------------------------------
 
-(defn on-snapshot-index-change [_ _ _ new-index]
-  (let [snapshot (@snapshot-list new-index)
+(defn )
+
+; TODO: Find closest index
+(defn on-selected-time-change [_ _ _ new-time]
+  (let [hour (.getHours new-time)
+        meridian (if (> 11 hour) "PM" "AM")
+        hour (if (> 12 hour) (- hour 12) hour)]
+    (val ($ "#hour") (pad hour))
+    (val ($ "#minute") (pad (.getMinutes new-time)))
+    (val ($ "#meridian") meridian)
+    (.datepicker ($ "#datepicker") "update" new-time))
+    )
+; CONTINUE HERE - REWORKING snapshots, selected-time atom
+
+  (let [snapshot (@snapshots new-index)
         time (Date. (:time snapshot))
         image (:image snapshot)
         time-display (str (.toDateString time) " " (.toLocaleTimeString time))]
@@ -88,76 +100,67 @@
     (prop ($ "#earlier") "disabled" (= (inc new-index) (count @snapshot-list)))
     (prop ($ "#later") "disabled" (zero? new-index))))
 
-(add-watch snapshot-index :_ on-snapshot-index-change)
-
-(defn on-selected-time-change [_ _ _ new-time]
-  (let [hour (.getHours new-time)
-        meridian (if (> 11 hour) "PM" "AM")
-        hour (if (> 12 hour) (- hour 12) hour)]
-    (val ($ "#hour") (pad hour))
-    (val ($ "#minute") (pad (.getMinutes new-time)))
-    (val ($ "#meridian") meridian)
-    (.datepicker ($ "#datepicker") "update" new-time)))
-
 (add-watch timestamp :_ on-selected-time-change)
 
 ;;------------------------------------------------------------------------------
 ;; Events
 ;;------------------------------------------------------------------------------
 
-(defn on-popstate! [])
+(defn on-popstate [e]
+  (if (.-state e)
+    (swap! selected-time #(.-time e))))
 
-(defn on-datetime-change! []
-  ); todo
+(defn on-datetime-change []
+  (let [date (.datepicker ($ "#datepicker") "getDate")
+        new-time (doto date
+                    (.setMinutes (.getMinutes @selected-time))
+                    (.setHours (.getHours @selected-time)))]
+    (swap! selected-time #(new-time))))
 
-(defn on-keydown! [e]
+(defn on-keydown [e]
   (let [key (.-which e)]
     (cond
-      (= key 37) (click-earlier!)
-      (= key 39) (click-later!))))
+      (= key 37) (click-earlier)
+      (= key 39) (click-later))))
 
-(defn click-toggle-meridian! []
+(defn click-toggle-meridian []
   (let [hours (+ (get-hours) 12)
         hours (if (>= 24 ) (- 24 hours) hours)]
     (set-hours! hours)))
 
-(defn click-decrement-hour! []
+(defn click-decrement-hour []
   (set-hours! (dec (get-hours))))
 
-(defn click-increment-hour! []
+(defn click-increment-hour []
   (set-hours! (inc (get-hours))))
 
-(defn click-decrement-Minute! []
+(defn click-decrement-Minute []
   (set-minutes! (dec (get-minutes))))
 
-(defn click-increment-minute! []
+(defn click-increment-minute []
   (set-minutes! (inc (get-minutes)))))
 
-(defn click-later! []
+(defn click-later []
   (if (pos? @snapshot-index)
-    (do
-      (swap! snapshot-index dec)
-      (sync-time-with-snapshot!))))
+    (use-index-time! (dec @snapshot-index))))
 
-(defn click-earlier! []
-  (if (>= @snapshot-index (count @snapshot-list))
-    (do
-      (swap! snapshot-index inc)
-      (sync-time-with-snapshot!))))
+(defn click-earlier []
+  (if (>= @snapshot-index (count @snapshot-list)
+    (use-index-time! (inc @snapshot-index)))))
 
 (defn add-events []
-  (aset js/window "onpopstate" on-popstate!)
-  (on ($ "datepicker") "changeDate" on-datetime-change!)
-  (on ($ js/document) "keydown" on-keydown!)
+  (aset js/window "onpopstate" on-popstate)
+  (on ($ "datepicker") "changeDate" on-datetime-change)
+  (on ($ js/document) "keydown" on-keydown)
 
   (-> ($ "body")
-    (on "click" ".toggle-meridian" click-toggle-meridian!)
-    (on "click" ".decrement-hour" click-decremen-hour!)
-    (on "click" ".increment-hour" click-increment-hour!)
-    (on "click" ".decrement-minute" click-decrement-Minute!)
-    (on "click" ".increment-minute" click-increment-minute!)
-    (on "click" "#later" click-later!)
-    (on "click" "#earlier" click-earlier!)))
+    (on "click" ".toggle-meridian" click-toggle-meridian)
+    (on "click" ".decrement-hour" click-decremen-hour)
+    (on "click" ".increment-hour" click-increment-hour)
+    (on "click" ".decrement-minute" click-decrement-Minute)
+    (on "click" ".increment-minute" click-increment-minute)
+    (on "click" "#later" click-later)
+    (on "click" "#earlier" click-earlier)))
 
 ;;------------------------------------------------------------------------------
 ;; App Init
@@ -172,7 +175,7 @@
         "endDate" end-date))))
 
 (defn init-timepicker []
-  )
+  (html ($ "timepickerContainer") (build-timepicker)))
 
 (defn init-snapshot-list []
   (swap! snapshot-list (fn []
